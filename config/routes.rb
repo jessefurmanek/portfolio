06cca2e895d1e7cdc396c3705ea7da028106a2ae
jessefurmanek <jesse.furmanek@gmail.com>
2015-03-15 16:40:39 -0500

    Add portfolio page and links to welcome, about, contact, and blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#welcome'
+
+  get '/portfolio' => 'portfolios#index'
+  get '/welcome' => 'pages#welcome'
+  get '/about' => 'pages#about'
+  get '/contact' => 'pages#contact'
+  get '/blog' => 'pages#blog'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
